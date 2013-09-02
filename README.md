b6dcf09abfcc4e98d952626cb9c766a47128631b
Chris Heng <bigblah@gmail.com>
2013-09-02 11:42:16 +0800

    DatabaseServiceProvider

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-silex-illuminate-database
-=========================
+DatabaseServiceProvider
+=======================
 
 Silex provider for the Illuminate Database component from the Laravel framework.
+
+License
+-------
+
+Released under the MIT license. See the LICENSE file for details.
---
+++ src/Gigablah/Silex/Database/DatabaseServiceProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Gigablah\Silex\Database;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Capsule;
+
+/**
+ * Integrates the Illuminate Database component from Laravel into Silex.
+ *
+ * @author Chris Heng <bigblah@gmail.com>
+ */
+class DatabaseServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['illuminate.db.default_options'] = array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'database',
+            'username'  => 'root',
+            'password'  => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        );
+
+        $app['illuminate.db'] = $app->share(function ($app) {
+            return $app['illuminate.capsule']->getConnection($app['illuminate.db.default_connection']);
+        });
+
+        $app['illuminate.db.options.initializer'] = $app->protect(function () use ($app) {
+            if (!isset($app['illuminate.db.options'])) {
+                $app['illuminate.db.options'] = array(
+                    'default' => array();
+                );
+            }
+
+            $tmp = $app['illuminate.db.options'];
+            foreach ($tmp as $name => &$options) {
+                $options = array_replace($app['illuminate.db.default_options'], $options);
+
+                if (!isset($app['illuminate.db.default_connection'])) {
+                    $app['illuminate.db.default_connection'] = $name;
+                }
+            }
+            $app['illuminate.db.options'] = $tmp;
+        });
+
+        $app['illuminate.capsule'] = $app->share(function ($app) {
+            $app['illuminate.db.options.initializer']();
+
+            $capsule = new Capsule();
+
+            foreach ($app['illuminate.db.options'] as $name => $options) {
+                $capsule->addConnection($options, $name);
+            }
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
